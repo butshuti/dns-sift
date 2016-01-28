bafa1c7011564fa615a61cb0dbfa14dc9ab658f9
Theogene <theogenebucuti@my.unt.edu>
2016-01-28 03:07:09 -0600

    Refactored python classifier modules to 'py' package.

--- src/classifier/src/dataloader.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def parse_file(fname):
-    with open(fname, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        ret = []
-        for row in reader:
-            ret.append(row)
-    return ret
